a23b7b0b9f722a518255f073ce834462e0e26323
Kenneth Shaw <kenneth.shaw@knq.io>
2016-06-08 16:37:27 +0700

    Adding quick hinfo tool and updating README.md

--- README.md
+++ README.md
@@ -13,3 +13,28 @@ go get -u github.com/knq/hilink
 # Usage
 
 Please see the godoc for api usage.
+
+# Notes
+
+This was built for interfacing with a Huawei E3370h-153 (specifically a Megafon
+M150-2) device, popular in Europe and Asia. It was flashed using a custom
+firmware and WebUI that enables the extra features.
+
+Here is the relevant information taken from the API using the
+[hinfo](cmd/hinfo) tool:
+```sh
+$ cd $GOPATH/src/github.com/knq/hilink
+$ go build ./cmd/hinfo/ ./hinfo
+{
+  "Classify": "hilink",
+  "DeviceName": "E3370",
+  "HardwareVersion": "CL2E3372HM",
+  <<sensitive information omitted>>
+  "Msisdn": "",
+  "ProductFamily": "LTE",
+  "SoftwareVersion": "22.200.09.01.161",
+  "WebUIVersion": "17.100.11.00.03-Mod1.0",
+  "supportmode": "LTE|WCDMA|GSM",
+  "workmode": "LTE"
+}
+```
---
+++ cmd/hinfo/hinfo.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/knq/hilink"
+)
+
+var (
+	flagEndpoint = flag.String("endpoint", "http://192.168.8.1/", "api endpoint")
+	flagDebug    = flag.Bool("v", false, "enable verbose")
+)
+
+func main() {
+	var err error
+
+	flag.Parse()
+
+	// options
+	opts := []hilink.Option{
+		hilink.URL(*flagEndpoint),
+	}
+	if *flagDebug {
+		opts = append(opts, hilink.Log(log.Printf, log.Printf))
+	}
+
+	// create client
+	client, err := hilink.NewClient(opts...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// get device info
+	d, err := client.DeviceInfo()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// change to json
+	v, err := json.MarshalIndent(d, "", "  ")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(os.Stdout, "%s\n", string(v))
+}
